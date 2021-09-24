abbd4064ce65f1285312a39fd542d614bd360337
Tomasz Ciecióra <tomek.cieciora@gmail.com>
2021-09-24 21:37:15 +0200

    Add project files.

---
+++ EmployeeData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string lastName;
+            int age;
+            string sex;
+            int pesel;
+            int employeeId;
+
+                
+            Console.WriteLine("podaj swoje imię");
+            name = Console.ReadLine();
+        Console.WriteLine(name);
+        }
+    }
+}
